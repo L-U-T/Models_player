57ca7b86f39b99bafd733227a190cd7f64956f60
Nerd <hndppt@outlook.com>
2022-11-03 18:42:00 +0800

    ADD(resources, main_player, wgpu_state) can base use
    - resources
      - light, shader, instance
    - main_player
      - base renderpass
    - wgpu_state
      - ...
    
    FOR PULL REQUEST

--- src/main_player/resources/model/mesh.rs
+++ src/main_player/resources/model/mesh.rs
@@ -3,12 +3,12 @@ use wgpu::util::DeviceExt;
 use super::vertex::ModelVertex;
 
 #[derive(Debug)]
-pub(crate) struct Mesh {
-    pub(crate) name: String,
-    pub(crate) vertex_buffer: wgpu::Buffer,
-    pub(crate) index_buffer: wgpu::Buffer,
-    pub(crate) num_elements: u32,
-    pub(crate) material: usize,
+pub struct Mesh {
+    pub name: String,
+    pub vertex_buffer: wgpu::Buffer,
+    pub index_buffer: wgpu::Buffer,
+    pub num_elements: u32,
+    pub material: usize,
 }
 
 impl Mesh {
